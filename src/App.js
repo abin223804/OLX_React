7f32230bcdb889b1ad1414e54660d480c856f5c4
Abin s chandran <abinschandran@gmail.com>
2023-09-15 18:32:40 +0530

    home and signup router done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
 
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
 
-import Home from './Pages/Home';
-
-function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/signup" exact component={Signup} />
+    </Router>
+  </div>
+);
 
 export default App;
