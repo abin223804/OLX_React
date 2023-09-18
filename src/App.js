9efbbf2907f21258f90203084f858e931d566a15
Abin s chandran <abinschandran@gmail.com>
2023-09-18 17:36:58 +0530

    name displayed in header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
-import React from "react";
+import React,{useEffect,useContext} from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
 import Login from './Pages/Login'
+import { AuthContext, FirebaseContext } from "./store/Context.1";
 
-const App = () => (
+function App  () {
+const {setUser} =useContext(AuthContext)
+const {firebase}=useContext(FirebaseContext)
+useEffect(()=>{
+  firebase.auth().onAuthStateChanged((user)=>{
+    setUser(user)
+  })
+})
+
+
+return(
   <div>
     <Router>
       <Route path="/" exact component={Home} />
@@ -15,5 +26,5 @@ const App = () => (
     </Router>
   </div>
 );
-
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { FirebaseContext} from "./store/FirebaseContext";
+import {FirebaseContext} from "./store/Context.1";
+import Context from "./store/Context.1"
 import firebase from './firebase/config'
 ReactDOM.render(
   <FirebaseContext.Provider value={{firebase}}>
+    <Context>
     <App />
+    </Context>
+   
   </FirebaseContext.Provider>,
 
   document.getElementById("root")
