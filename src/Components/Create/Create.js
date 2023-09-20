0fbe74fac2f67fa7444ee341e0a168f564b5c95e
Abin s chandran <abinschandran@gmail.com>
2023-09-20 19:13:36 +0530

    form tags removed

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,66 +1,75 @@
-import React, { Fragment, useState } from 'react';
-import './Create.css';
-import Header from '../Header/Header';
+import React, { Fragment, useState } from "react";
+import "./Create.css";
+import Header from "../Header/Header";
 
 const Create = () => {
-
-const[name,setName]=useState('');
-const[category,setCategory]=useState('');
-const[price,setPrice]=useState('');
-const[image,setImage]=useState(null);
-
-
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
 
   return (
     <Fragment>
       <Header />
       <card>
         <div className="centerDiv">
-          <form>
-            <label htmlFor="fname">Name</label>
-            <br />
-            <input
-              className="input"
-              type="text"
-              value={name}
-              onChange={(e)=>{
-                setName(e.target.value);
-              }}
-              id="fname"
-              name="Name"
-              defaultValue="John"
-            />
-            <br />
-            <label htmlFor="fname">Category</label>
-            <br />
-            <input
-              className="input"
-              type="text"
-              value={category}
-              onChange={(e)=>{
-                setCategory(e.target.value);
-              }}
-              id="fname"
-              name="category"
-              defaultValue="John"
-            />
-            <br />
-            <label htmlFor="fname"
-            >Price</label>
-            <br />
-            <input className="input" type="number" value={price} onChange={(e)=>setPrice(e.target.value)} id="fname" name="Price" />
-            <br />
-          </form>
+          <label htmlFor="fname">Name</label>
           <br />
-          <img alt="Posts" width="200px" height="200px" src={image? URL.createObjectURL(image):''}></img>
-          <form>
-            <br />
-            <input type="file" onChange={(e)=>{
+          <input
+            className="input"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            id="fname"
+            name="Name"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="fname">Category</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            value={category}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+            id="fname"
+            name="category"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="fname">Price</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            id="fname"
+            name="Price"
+          />
+          <br />
+
+          <br />
+          <img
+            alt="Posts"
+            width="200px"
+            height="200px"
+            src={image ? URL.createObjectURL(image) : ""}
+          ></img>
+
+          <br />
+          <input
+            type="file"
+            onChange={(e) => {
               setImage(e.target.files[0]);
-            }} />
-            <br />
-            <button className="uploadBtn">upload and Submit</button>
-          </form>
+            }}
+          />
+          <br />
+          <button className="uploadBtn">upload and Submit</button>
         </div>
       </card>
     </Fragment>
