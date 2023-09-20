2eed44e59f286fae277db10bdebf08f55b31140c
Abin s chandran <abinschandran@gmail.com>
2023-09-20 15:50:35 +0530

    loginpage link added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,18 @@
-import React,{useContext} from 'react';
-import {useHistory}from 'react-router-dom'
+import React, { useContext } from "react";
+import { useHistory, Link } from "react-router-dom";
 
-import './Header.css';
-import OlxLogo from '../../assets/OlxLogo';
-import Search from '../../assets/Search';
-import Arrow from '../../assets/Arrow';
-import SellButton from '../../assets/SellButton';
-import SellButtonPlus from '../../assets/SellButtonPlus';
-import { AuthContext, FirebaseContext } from '../../store/Context.1';
+import "./Header.css";
+import OlxLogo from "../../assets/OlxLogo";
+import Search from "../../assets/Search";
+import Arrow from "../../assets/Arrow";
+import SellButton from "../../assets/SellButton";
+import SellButtonPlus from "../../assets/SellButtonPlus";
+
+import { AuthContext, FirebaseContext } from "../../store/Context.1";
 function Header() {
-const history=useHistory();
-const {user} =useContext(AuthContext)
-const {firebase} =useContext(FirebaseContext)
+  const history = useHistory();
+  const { user } = useContext(AuthContext);
+  const { firebase } = useContext(FirebaseContext);
 
   return (
     <div className="headerParentDiv">
@@ -39,18 +40,28 @@ const {firebase} =useContext(FirebaseContext)
           <span> ENGLISH </span>
           <Arrow></Arrow>
         </div>
-        <div className="loginPage">
-          <span>{user ? `Welcome ${user.displayName}` : 'Login'}</span>
-          
-          <hr />
-          </div>
 
-          {user && <span onClick={()=>{
-            firebase.auth().signOut();
-            history.push('/login')
-          }}>Logout</span>}
-          
-       
+        <div className="loginPage">
+          {user ? (
+            `Welcome ${user.displayName}`
+          ) : (
+            <Link to="Login" style={{ color: "black" }}>
+              Login
+            </Link>
+          )}
+          <hr />
+        </div>
+
+        {user && (
+          <span
+            onClick={() => {
+              firebase.auth().signOut();
+              history.push("/login");
+            }}
+          >
+            Logout
+          </span>
+        )}
 
         <div className="sellMenu">
           <SellButton></SellButton>
